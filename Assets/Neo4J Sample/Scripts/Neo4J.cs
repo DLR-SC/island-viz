da492431481dc8f8acee4ffcc81192e57b8632d2
David Heidrich <daviheid@web.de>
2019-07-15 14:38:44 +0200

    added neo4j driver

---
+++ Assets/Neo4J Sample/Scripts/Neo4J.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using Neo4j.Driver;
+using System;
+using Neo4j.Driver.V1;
+
+namespace Neo4JExample
+{
+    // The following example is based on the HelloWorldExample on https://neo4j.com/developer/dotnet/.
+
+    public class Neo4J : IDisposable
+    {
+        // Connection with a Neo4J database, providing a access point via the ISession method. 
+        private readonly IDriver driver;
+        private bool debug;
+
+        // The class constructor.
+        public Neo4J(string uri, string user, string password, bool debugLogs)
+        {
+            // New IDriver instance.
+            driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+            // Optional debug lines
+            debug = debugLogs;
+            if (debug)
+            {
+                Debug.Log("Neo4j .NET driver: Connected to Neo4J database " + driver.Uri + " with username " 
+                    + user);
+            }
+        }
+
+        // Executes a given command and returns the result in a IStatementResult.
+        // A command must be a string representing a Cypher statement.
+        // An example for a command would be "MATCH (cloudAtlas {title: \"Cloud Atlas\"}) RETURN 
+        // cloudAtlas.released".
+        public IStatementResult Transaction (string command)
+        {
+            if (debug)
+            {
+                Debug.Log("Neo4j .NET driver: Executing statement #" + command + "#");
+            }            
+            using (var session = driver.Session())
+            {
+                return session.WriteTransaction(tx =>
+                {
+                    // Runs the statement and returns a result.
+                    return tx.Run(command);
+                });
+            }
+        }
+
+        // Implement IDisposable.
+        public void Dispose()
+        {
+            // Dispose managed resources.
+            driver?.Dispose();
+        }
+    }
+}
+
+
---
+++ Assets/Neo4J Sample/Scripts/Neo4J_Example.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Neo4j.Driver.V1;
+using System.Linq;
+
+
+
+namespace Neo4JExample
+{
+    // This example is using the movie example neo4j database.
+
+    public class Neo4J_Example : MonoBehaviour
+    {
+
+        Neo4JExample.Neo4J dataBase;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            dataBase = new Neo4JExample.Neo4J("bolt://localhost:7687", "neo4j", "123", true);
+        }
+
+        // An example with a Cypher statement returning one release date.
+        public void SingleItemExample ()
+        {
+            IStatementResult result = dataBase.Transaction("MATCH (cloudAtlas {title: \"Cloud Atlas\"}) " +
+                "RETURN cloudAtlas.released");
+            string release = result.Single()[0].As<string>();
+            Debug.Log(release);
+        }
+
+        // An example with a Cypher statement returning multiple movie titles.
+        public void MultipleItemsExample ()
+        {
+            IStatementResult result = dataBase.Transaction(
+                "MATCH (nineties:Movie) WHERE nineties.released >= 1990 AND nineties.released < 2000 " +
+                "RETURN nineties.title");
+
+            // Convert the IStatementResult to a IRecord List.
+            var results = result.ToList();
+
+            // Debug 
+            Debug.Log("Neo4F_Example: " + results.Count + " results were found!");
+
+            // Iterates through all results.
+            for (int i = 0; i < results.Count; i++)
+            {
+                // Print the result to the console.
+                Debug.Log(results[i].Values.As<List<string>>()[0]);
+            }
+        }
+
+        // An example with a Cypher statement returning multiple person names and movie titles.
+        public void MultipleItemsExample2()
+        {
+            IStatementResult result = dataBase.Transaction(
+                "MATCH (tom:Person {name: \"Tom Hanks\"})-[:ACTED_IN]->(tomHanksMovies) " +
+                "RETURN tom.name,tomHanksMovies.title");
+
+            // Convert the IStatementResult to a IRecord List.
+            var results = result.ToList();
+
+            // Debug 
+            Debug.Log("Neo4F_Example: " + results.Count + " results were found!");
+
+            for (int i = 0; i < results.Count; i++)
+            {
+                foreach (var item in results[i].Values.As<List<string>>())
+                {
+                    // Print the result to the console.
+                    Debug.Log(item);
+                }
+            }
+        }
+
+    }
+}
